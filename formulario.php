82cadb824cd55480ba0cad152985e1de22dcf85c
kobrastunt <45412@a.iesleopoldoqueipo>
2024-04-05 22:04:44 +0200

    formulario

--- formulario.php
+++ formulario.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Insertar Imagen</title>
+<title>Insertar personaje</title>
 <style>
     body {
         font-family: Arial, sans-serif;
